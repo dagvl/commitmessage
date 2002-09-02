4fdb71da0061f519fbdb837d7c007a5814d2baa3
shaberman <shaberman>
2002-09-02 20:07:54 +0000

    Two sample views.

---
+++ ViewBombsight.pm
@@ -0,0 +1,90 @@
+#!/usr/bin/perl -w
+#
+# ViewBombsight.pm
+# Version: 2.0
+#
+# Sample script that sends out notification of bug-related commits
+# to Bombsight.
+#
+# (Bombsight is a slight modification to the bug tracking system
+# FogBUGZ <http://www.fogcreek.com/FogBUGZ/>).
+#
+
+package ViewBombsight;
+use Socket;
+use strict;
+
+#
+# Creates a new view object.
+#
+sub new {
+    my($type, $props, $om) = @_;
+    my $self = {};
+    bless($self, $type);
+
+    # Copy over props, and eval them
+    foreach my $key (keys %$props) {
+        eval("\$self->{$key} = \"$props->{$key}\"");
+    }
+
+    return $self;
+}
+
+#
+# Do nothing on a newdir
+#
+sub newdir {
+    my($self, $om) = @_;
+
+    return;
+}
+
+#
+# Post the details of each affected files to Bombsight
+#
+sub commit {
+    my($self, $om) = @_;
+
+    # Get the bug id
+    my $bugId = 0;
+    my $log = $om->{log};
+
+    if ($log =~ /b(0|o)mb((site)|(sight)):\s*([0-9]*)/i) {
+        $bugId = int($5);
+        print "Adding bug info for bug $bugId...\n";
+    }
+    elsif ($log =~ /fogbug[sz]?\s*id\s*:\s*([0-9]*)/i) {
+        $bugId = int($1);
+        print "Adding bug info for bug $bugId...\n";
+    }
+
+    if ($bugId > 0) {
+        foreach my $file (keys %{$om->{files}}) {
+            my $action = $om->{files}{$file}{action};
+
+            my $get = "";
+            $get .= "GET $self->{url}$self->{page}";
+            $get .= "?ixBug=$bugId";
+            $get .= "&sFile=/$om->{module}/$file";
+            $get .= "&sPrev=$om->{files}{$file}{prev_rev}";
+            $get .= "&sNew=$om->{files}{$file}{rev}";
+            $get .= " HTTP/1.1\n";
+
+            $get .= "Host: $self->{server}:$self->{port}\n\n";
+
+##            print "GET = \n$get";
+
+            my $in_addr = (gethostbyname($self->{server}))[4] || die("Error: $!\n");
+            my $paddr = sockaddr_in($self->{port}, $in_addr) || die("Error: $!\n");
+            socket(S, PF_INET, SOCK_STREAM, getprotobyname('tcp')) || die("Error: $!\n");
+            connect(S, $paddr) || die("Error: $!\n");
+            select(S);
+            $|=1;
+            select(STDOUT);
+            print S $get;
+            close(S);
+        }
+    }
+}
+
+1;
---
+++ ViewEmail.pm
@@ -0,0 +1,164 @@
+#!/usr/bin/perl -w
+#
+# ViewEmail.pm
+# Version: 2.0
+#
+# Simple email script that acts as a view for the MVC-based
+# commitmessage script.
+#
+# TODO: 
+#
+# - Move the bug id replacement stuff to the config file
+#
+
+package ViewEmail;
+use strict;
+
+my @DAYS = ( 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' );
+my @MONTHS = ( 'January', 'Febuary', 'March', 'April', 'May', 'June', 'July',
+        'August', 'September', 'October', 'November', 'December' );
+
+#
+# Creates a new view object.
+#
+sub new {
+    my($type, $props, $om) = @_;
+    my $self = {};
+    bless($self, $type);
+
+    # Copy over props, and eval them
+    foreach my $key (keys %$props) {
+        eval("\$self->{$key} = \"$props->{$key}\"");
+    }
+
+    my @time = gmtime();
+    $self->{shortdatetime} = sprintf("%s %s %s %s %s:%s:%s", 
+        $DAYS[$time[6]], $time[3], $MONTHS[$time[4]],
+        $time[5] + 1900, $time[2], $time[1], $time[0]);
+    $self->{datetime} = "$self->{shortdatetime} -0000 (GMT)";
+
+    return $self;
+}
+
+#
+# Send a simple email on newdir creation
+#
+sub newdir {
+    my($self, $om) = @_;
+
+    $self->sendmail($om, "<p>$om->{user} created $om->{newdir}</p>");
+}
+
+#
+# Send a list of affected files and diffs on commit
+#
+sub commit {
+    my($self, $om) = @_;
+
+    # First remove all of the <, >, and \n
+    my $log = $self->formatlog($om->{log});
+
+    # Basic header    
+    my $text = "<p>$om->{user} committed the following changes on $self->{shortdatetime}.</p>";
+
+    # Create the the table header
+    $text .= "<p>\n";
+    $text .= "<table border=1 cellspacing=0>\n";
+    $text .= "   <tr>\n";
+    $text .= "      <td colspan=5>&nbsp;<br>$log<br>&nbsp;<br></td>\n";
+    $text .= "   </tr>\n";
+    $text .= "   <tr>\n";
+    $text .= "      <td><center>File</center></td>\n";
+    $text .= "      <td><center>Rev</center></td>\n";
+    $text .= "      <td><center>Lines</center></td>\n";
+    $text .= "      <td><center>State</center></td>\n";
+    $text .= "   </tr>\n";
+
+    # Give a one-line description for each file
+    foreach my $file (sort keys %{$om->{files}}) {
+        my $action = $om->{files}{$file}{action};
+
+        $text .= "   <tr>\n";
+        $text .= "      <td><nobr><a href=\"$self->{cvsweb}/$om->{module}/$file\">$file</a>&nbsp;</nobr></td>\n";
+        $text .= "      <td><center>$om->{files}{$file}{rev}</center></td>\n";
+        $text .= "      <td><nobr><center>$om->{files}{$file}{delta}</center></nobr></td>\n";
+        $text .= "      <td><center>$action</center></td>\n";
+        $text .= "   </tr>\n";
+    }
+
+    $text .= "</table></p>\n\n";
+
+    # Go through the diffs of the added/modified files
+    $text .= "<p>Diffs:</p>\n";
+    foreach my $file (sort keys %{$om->{files}}) {
+        my $action = $om->{files}{$file}{action};
+        if ($action eq "added" || $action eq "modified") {
+            my $diff = $om->{files}{$file}{diff};
+            $diff =~ s/</&lt;/g;
+            $diff =~ s/>/&gt;/g;
+
+            $text .= "<p><pre>$diff</pre></p>&nbsp<br />\n";
+        }
+    }
+
+    $text .= "\n\n";
+
+    $self->sendmail($om, $text);
+}
+
+#
+# Format the log to HTML and handle bombsight links
+#
+sub formatlog {
+    my($self, $log) = @_;
+
+    $log = $self->toHtml($log);
+    $log =~ s/b(0|o)mb((site)|(sight)):\s*([0-9]*)/<p><a href="$self->{bombsight}\/editbug.asp?command=view&ixBug=$5">Fixed for bug $5<\/a>/i;
+
+    return $log;
+}
+
+#
+# Convert <, >, \n to HTML
+#
+sub toHtml {
+    my($self, $text) = @_;
+
+    $text =~ s/</&lt;/g;
+    $text =~ s/>/&gt;/g;
+    $text =~ s,\n,&nbsp;<br />,g;
+
+    return $text;
+}
+
+#
+# Perform the sendmail operation
+#
+sub sendmail {
+    my($self, $om, $text) = @_;
+
+    # Don't send if 'to' is not set
+    if (!defined($self->{to})) {
+        print "Skipping cvs email...\n";
+        return;
+    }
+
+    print "Sending cvs email...\n";
+
+    open(MAIL, "|$self->{sendmail}") || die("could not open mail '$self->{sendmail}': $!");
+
+    print MAIL "To: $self->{to}\n";
+    print MAIL "From: $self->{from}\n";
+    print MAIL "Subject: $self->{subject}\n";
+    print MAIL "MIME-Version: 1.0\n";
+    print MAIL "Content-Type: text/html\n";
+    print MAIL "Date: " . $self->{datetime} . "\n\n";
+
+    print MAIL $self->{header};
+    print MAIL $text;
+    print MAIL $self->{footer};
+
+    close(MAIL);
+}
+
+1;
