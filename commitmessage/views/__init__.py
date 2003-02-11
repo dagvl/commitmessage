8dcbb14f224dd3e152b1bb877aad45504e4a2e37
shaberman <shaberman>
2003-02-11 07:06:24 +0000

    Added simple text to fill out __init__.py.

---
+++ commitmessage/views/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#
+# commitmessage.py Version 2.0-alpha1
+# Copyright 2002 Stephen Haberman
+# 
+
+"""Views work against the object model to send out information to outside systems."""
