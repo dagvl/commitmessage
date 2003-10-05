56c6fabe5f1d9f3e53f8fcb6d3a5f7935ea995ee
shaberman <shaberman>
2003-10-05 18:47:06 +0000

    Use configFile instead of confFile.

--- commitmessage/main.py
+++ commitmessage/main.py
@@ -21,21 +21,22 @@ from commitmessage.exceptions import CmException
 from commitmessage.util import CmConfigParser, getNewInstance
 
 if __name__ == '__main__':
-    confFile = 'commitmessage.conf'
+    configFile = 'commitmessage.conf'
 
     options, args = getopt.getopt(sys.argv[1:], "c:")
     for option, value in options:
         if option == "-c":
-            confFile = value
+            configFile = value
 
-    if confFile[0] != '/' and confFile[0] != '.':
-        confFile = os.path.abspath(os.path.dirname(sys.argv[0])) + '/' + confFile
-    conf = CmConfigParser(confFile)
+    """Handle relative paths."""
+    # if configFile[0] != '/' and configFile[0] != '.':
+    #    configFile = os.path.abspath(os.path.dirname(sys.argv[0])) + '/' + configFile
+    config = CmConfigParser(os.path.realpath(configFile))
 
-    scm = conf.get('scm', 'interface')
+    scm = config.get('scm', 'interface')
     controller = getNewInstance(scm)
 
     cleanArgs = [sys.argv[0]]
     cleanArgs.extend(args)
-    controller.__init__(conf, cleanArgs, sys.stdin)
+    controller.__init__(config, cleanArgs, sys.stdin)
     controller.process()
