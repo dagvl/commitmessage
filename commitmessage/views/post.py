e62de9930817d60755261d4533363998dffacd6a
shaberman <shaberman>
2003-10-09 01:14:21 +0000

    Should make a generic POST library that handles FogBugz type stuff.

---
+++ commitmessage/views/post.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#
+# commitmessage 2.0-alpha1
+# Copyright 2002 Stephen Haberman
+#
+
+"""Provides hooks into various systems via HTTP post. """
+
+import os
+import sys
+
+from commitmessage.framework import View
+
+class PostView(View):
+    pass
