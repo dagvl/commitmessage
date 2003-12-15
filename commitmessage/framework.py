9e3cec0e40234ba88124ff41aba818e4b1b9e1ce
shaberman <shaberman>
2003-12-15 19:29:22 +0000

    Updated to use the new attribute method and property idiom so that properties are well documented.

--- commitmessage/framework.py
+++ commitmessage/framework.py
@@ -1,33 +1,44 @@
 #!/usr/bin/env python
 #
-# commitmessage.py Version 2.0-alpha1
-# Copyright 2002, 2003 Stephen Haberman
+# commitmessage
+# Copyright 2002-2003 Stephen Haberman
 #
 
-"""Provides the core classes for the commitmessage framework."""
+"""
+Provides the model classes for the commitmessage framework.
+"""
 
 import os
 import string
 import sys
 
 from commitmessage.exceptions import CmException
+from commitmessage.attribute import attribute
 
 class Controller:
-    """A base implementation for SCM-specific controllers to extend; mostly
-    does the generic Views handling."""
+    """
+    A base implementation for SCM-specific controllers to extend; mostly
+    does the generic Views handling.
+    """
 
+    # A temp directory to dump the diff files to.
     TMPDIR = os.environ.get('TMP', None) or os.environ.get('TEMP', None) or '/tmp'
-    """A temp directory to dump the diff files to."""
 
     def __init__(self, config, argv, stdin):
-        """Initializes the controller's Model and saves the references to argv and stdin."""
+        """
+        Initializes the controller's model and saves the references to argv and stdin.
+
+        @param config: a L{commitmessage.util.CmConfigParser} created by L{commitmessage.main}
+        @param argv: the command line arguments, as gathered by L{commitmessage.main}
+        @param stdin: where to stream responses to
+        """
         self.config = config
         self.argv = argv
         self.stdin = stdin
         self.model = Model()
 
     def process(self):
-        """Starts the generalized commitmessage process of building the model and executing the views."""
+        """Starts the SCM-agnostic process of building the model and executing the views."""
         self._populateModel()
 
         # Allow cvs to halt the process as it's data is cached between per-directory executions
@@ -37,140 +48,125 @@ class Controller:
         self._executeViews()
 
     def _populateModel(self):
-        """This should be over-ridden by concrete controllers with their implementation-specific logic."""
+        """
+        This should be overridden by concrete controllers with their
+        implementation-specific logic for populating the model.
+        """
         raise CmException('The controller did not override the _populateModel method.')
 
     def _stopProcessForNow(self):
-        """Gives CVS a chance to kill the current process and wait for the next
-        one as it has to build up data between discrete per-directory executions."""
+        """
+        Gives CVS a chance to kill the current process and wait for the next
+        one as it has to build up data between discrete per-directory executions.
+        """
         return 0
 
     def _executeViews(self):
-        """Fires off the views for each module that matches the commits base directory."""
+        """Executes the views for each module that matches the commit's base directory."""
         modules = self.config.getModulesForPath(self.model.greatestCommonDirectory())
         for module in modules:
             views = self.config.getViewsForModule(module, self.model)
             for view in views:
                 view.execute()
 
-class DomainObject:
-    """Puts in some getter/setter infrastructure."""
-
-    def __init__(self, readonly={}):
-        # Use __dict__ to avoid going to __setattr__
-        self.__dict__['readonly'] = readonly
-        for key, value in self.readonly.items():
-            self.__dict__[key] = value
-
-    def _upper(self, name):
-        return name[0].upper() + name[1:]
-
-    def __setattr__(self, name, value):
-        """Optionally delegate checking out to setters."""
-
-        if self.readonly.has_key(name):
-            raise CmException, 'The attribute %s is read only.' % name
-
-        setter = 'set%s' % self._upper(name)
-        if hasattr(self, setter):
-            method = getattr(self, setter)
-            if callable(method):
-                method(value)
-                return
-
-        # There was no setter, go ahead and accept the attribute
-        self.__dict__[name] = value
-
-    def __getattr__(self, name):
-        """Does some simple calculated values."""
-        getter = 'get%s' % self._upper(name)
-        if hasattr(self, getter):
-            method = getattr(self, getter)
-            if callable(method):
-                return method()
-
-        # There was no getter to calculate the value, so fall back with it not being a valid attribute
-        raise AttributeError, 'Attribute %s does not have a valid getter or simply does not exist.' % name
-
-class File(DomainObject):
+class File:
     """Represents a file that has been affected by the commit."""
 
     def __init__(self, name, directory, action):
-        """Files must be owned by a parent directory; by virtue of being created, this file is added to its parent directory."""
+        """
+        @param name: the file name, e.g. C{foo.txt}
+        @param directory: the parent directory, the file will automatically add itself to this directory
+        @param action: the action performed on this file that required a commit, SCM-dependent
+        """
         if name.find('/') != -1:
             raise CmException, "File names may not have foward slashes in them."
 
-        # Set up the read only attributes
-        DomainObject.__init__(self, {'name': name, 'directory': directory})
-
-        self.action = action
-        self.rev = None
-        self.delta = None
-        self.diff = None
+        self._name = name
+        self._directory = directory
+        self._action = action
+        self._rev = None
+        self._delta = None
+        self._diff = None
 
         self.directory.addFile(self)
 
+    name = attribute('_name', permit='r', doc="""The file name (C{foo.txt})""")
+    directory = attribute('_directory', permit='r', doc="""The L{Directory} the file is in""")
+    action = attribute('_action', permit='r', doc="""The action on thie file that required a commit, SCM-dependent""")
+    rev = attribute('_rev', doc="""The revision number (SCM-dependent, could be per-file or per-commit) for this file""")
+    delta = attribute('_delta', doc="""The number of lines added/removed/changed in this commit""")
+    diff = attribute('_diff', doc="""The diff of what changed in the file""")
+
     def __str__(self):
-        """Returns the path of the file."""
+        """@return: the path of the file"""
         return '<File %s>' % self.path
 
-    def getPath(self):
-        return self.directory.path + self.name
+    def path():
+        doc = """The full path (C{/dir/foo.txt}) of the file"""
+        def fget(self):
+            return self.directory.path + self.name
+        return locals()
+    path = property(**path())
 
-class Directory(DomainObject):
+class Directory:
     """Represents a directory that has been affected by the commit."""
 
     def __init__(self, path, action='none'):
         if path == '' or not path.startswith('/') or not path.endswith('/'):
             raise CmException, 'Directory paths must start with a forward slash and end with a forward slash.'
 
-        # Setup the read only attribute
-        DomainObject.__init__(self, {'path': path, 'files': [], 'subdirectories': []})
+        self._path = path
+        self._action = action
+        self._files = []
+        self._subdirectories = []
 
-        self.action = action
+    path = attribute('_path', permit='r', doc="""The full path of the directory (C{/dir/dir/})""")
+    action = attribute('_action', permit='r', doc="""The action performed on this directory that caused the commit""")
+    files = attribute('_files', permit='r', doc="""The L{File}s within this directory affected by the commit""")
+    subdirectories = attribute('_subdirectories', permit='r', doc="""The L{Directory}s within this directory affected by the commit""")
 
-    def __repr__(self):
-        """Return the path of the directory."""
-        return """<Directory '%s'>""" % self.path
+    def name():
+        doc = """The name of the directory (C{dir} with no slashes)"""
+        def fget(self):
+            if self.path == '/':
+                return '/'
+            else:
+                # The path will be /aaa/bbb/name/
+                return self.path.split('/')[-2]
+        return locals()
+    name = property(**name())
 
     def __str__(self):
-        return self.__repr__()
-
-    def getName(self):
-        """Returns just the name of the directory, e.g. testdir with no slashes."""
-        if self.path == '/':
-            return '/'
-        else:
-            # The path will be /aaa/bbb/name/
-            return self.path.split('/')[-2]
+        """@return: the path of the directory"""
+        return """<Directory '%s'>""" % self.path
 
     def filesByAction(self, action):
-        """Returns all of the files in this directory affected by the commit."""
+        """@return: all of the files in this directory affected by the commit"""
         if action == None:
             return self.files
         else:
             return filter(lambda file: file.action == action, self.files)
 
     def file(self, name):
-        """Return a file in the current directory with the given name, or else None if it doesn't exist."""
+        """@return: the file in the current directory with C{name} or C{None}"""
         for file in self.files:
             if file.name == name:
                 return file
         return None
 
     def subdirectory(self, name):
-        """Return a subdirectory with a specific name."""
+        """@return: the subdirectory with C{name} or C{None}"""
         for dir in self.subdirectories:
             if dir.name == name:
                 return dir
         return None
 
     def hasSubdirectory(self, name):
-        """Returns whether the directory has a subdirectory named 'name'."""
+        """@return: whether the directory has a subdirectory with C{name}."""
         for dir in self.subdirectories:
             if dir.name == name:
-                return 1
-        return 0
+                return True
+        return False
 
     def addFile(self, file):
         """Saves a file object into this directory."""
@@ -180,18 +176,22 @@ class Directory(DomainObject):
         """Saves a directory object into this directory."""
         self.subdirectories.append(subdir)
 
-class Model(DomainObject):
-    """Wraps the model that gets sent between the Controller and Views.
-
-    Properties are available via accessors to allow easy documentation."""
+class Model:
+    """
+    Wraps the model that gets sent between the Controller and Views.
+    """
 
     def __init__(self):
-        DomainObject.__init__(self, {'rootDirectory': Directory('/')})
-        self.user = ''
+        self._rootDirectory = Directory('/')
+        self._user = ''
+
+    rootDirectory = attribute('_rootDirectory', permit='r', doc="""The root directory within the SCM""")
+    user = attribute('_user', doc="""The user performing the commit""")
 
     def directory(self, path):
-        """Returns the Model's corresponding directory for the given path.
-        Creates the new directories and its sub hierarchy if needed."""
+        """
+        @return: the model's L{Directory} for C{path}; creates new directories and sub directories if needed
+        """
         if path[0] != '/' or path[-1] != '/':
             raise CmException, 'Directory paths must start with a forward slash and end with a forward slash.'
         if path == '/':
@@ -211,9 +211,12 @@ class Model(DomainObject):
         return currentDirectory
 
     def addDirectory(self, directory):
-        """Adds a directory into the Model's directory tree along. Uses
-        self.directory(path), except takes a default value for the new directory
-        to add at the bottom of the hierarchy."""
+        """
+        Adds C{directory} into the model's directory tree.
+
+        Uses C{self.directory(path)}, except it takes a default value for the
+        new directory to add at the bottom of the hierarchy.
+        """
         # Leave off last name and last slash to get the parent directory
         parentPath = '/'.join(directory.path.split('/')[0:-2]) + '/'
         parentDirectory = self.directory(parentPath)
@@ -221,7 +224,7 @@ class Model(DomainObject):
             parentDirectory.addSubdirectory(directory)
 
     def greatestCommonDirectory(self):
-        """The greatest commont directory of a commit to base the module matching on."""
+        """@return: the greatest common directory of the commit to base the module matching on"""
         dir = self.rootDirectory
         while dir.action == 'none' \
             and len(dir.subdirectories) == 1 \
@@ -230,7 +233,7 @@ class Model(DomainObject):
         return dir.path
 
     def file(self, path):
-        """Returns the file for the given path."""
+        """@return: the L{File} for the C{path}"""
         if path == '':
             raise CmException, "File paths may not be empty."
         if path[0] != '/' or path[-1] == '/':
@@ -243,8 +246,7 @@ class Model(DomainObject):
         return self.directory(parentDirectoryPath).file(parts[-1])
 
     def files(self, action=None):
-        """Returns a flat list of files with the optional filter applied to
-        their action."""
+        """@return: a flat list of L{File}s, optionally those that match C{action}"""
         return self._files(self.rootDirectory, action)
 
     def _files(self, directory, action):
@@ -261,7 +263,7 @@ class Model(DomainObject):
         return files
 
     def directories(self, action=None):
-        """Returns a flat list of directories with the optional filter applied to their action."""
+        """@return: a flat list of L{Directory}s, optionally those that match C{action}."""
         return self._directories(self.rootDirectory, action)
 
     def _directories(self, directory, action):
@@ -277,12 +279,14 @@ class Model(DomainObject):
         return dirs
 
     def directoriesWithFiles(self, action=None):
-        """Returns a flat list of directories that have changes to files in them."""
+        """@return: a flat list of L{Directory}s that have changes to files"""
         return self._directoriesWithFiles(self.rootDirectory, action)
 
     def _directoriesWithFiles(self, directory, action):
-        """Internal helper method to recursively build a flat list of
-        directories that contain changes to files in them."""
+        """
+        Internal helper method to recursively build a flat list of directories
+        that contain changes to files in them.
+        """
         dirs = []
         if len(directory.filesByAction(action)) > 0:
             dirs.append(directory)
@@ -319,3 +323,4 @@ def _test():
 
 if __name__ == '__main__':
     _test()
+
