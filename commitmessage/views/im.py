f416b81aa2151c15f316ae8225ab6e96871a34b4
shaberman <shaberman>
2005-09-03 03:13:02 +0000

    Patch by Brian Davis (brian.william.davis@gmail.com):
    * commitmessage/views/im.py
      (MSNView, AIMView): Enclosed view classes and imported modules in a "try...except" block to prevent failure if the user doesn't have the module installed.

--- commitmessage/views/im.py
+++ commitmessage/views/im.py
@@ -9,8 +9,6 @@
 import time
 
 from StringIO import StringIO
-from toc import TocTalk, BotManager
-import msnp
 
 from commitmessage.model import View
 
@@ -45,63 +43,70 @@ class IMView(View):
         text.seek(0)
         return text.read()
 
-class MSNView(IMView):
-    """Sends out commit messages on MSN
+try:
+    import msnp
+    class MSNView(IMView):
+        """Sends out commit messages on MSN
+    
+        This view has three properties:
+    
+         - passport - the MSN passport to login with
+         - password - the password for the MSN passport
+         - to - a comma-separated list of other MSN passports to send the message to
+        """
+    
+        def execute(self):
+            """Sends the IM with the commit message"""
+    
+            class MsnListener(msnp.SessionCallbacks):
+                def __init__(self, message):
+                    self._message = message
+                    self._done = False
+                def chat_started(self, chat):
+                    chat.send_message(self._message)
+                    self._done = True
+    
+            listener = MsnListener(self._generateMessage())
+            msn = msnp.Session(listener)
+    
+            msn.login(self.passport, self.password)
+    
+            for name in [name.strip() for name in self.to.split(',')]:
+                msn.start_chat(name)
+                listener._done = False
+    
+                while not listener._done:
+                    msn.process(chats=True)
+                    time.sleep(1)
+except:
+    pass
 
-    This view has three properties:
-
-     - passport - the MSN passport to login with
-     - password - the password for the MSN passport
-     - to - a comma-separated list of other MSN passports to send the message to
-    """
-
-    def execute(self):
-        """Sends the IM with the commit message"""
-
-        class MsnListener(msnp.SessionCallbacks):
-            def __init__(self, message):
-                self._message = message
-                self._done = False
-            def chat_started(self, chat):
-                chat.send_message(self._message)
-                self._done = True
-
-        listener = MsnListener(self._generateMessage())
-        msn = msnp.Session(listener)
-
-        msn.login(self.passport, self.password)
-
-        for name in [name.strip() for name in self.to.split(',')]:
-            msn.start_chat(name)
-            listener._done = False
-
-            while not listener._done:
-                msn.process(chats=True)
-                time.sleep(1)
-
-class AIMView(IMView):
-    """Sends out commit messages on AIM
-
-    This view has three properties:
-
-     - screenname - the AIM screenname to login with
-     - password - the password for AIM screenname
-     - to - a comma-seperated list of other AIM screennames to send the message to
-    """
-
-    def execute(self):
-        """Sends the IM with the commit message"""
-
-        bm = BotManager()
-        bot = TocTalk(self.screenname, self.password)
-
-        bm.addBot(bot, "bot")
-        time.sleep(4)
-
-        message = self._generateMessage()
-
-        names = [name.strip() for name in self.to.split(',')]
-        for name in names:
-            print name
-            bot.do_SEND_IM(name, message)
-
+try:
+    from toc import TocTalk, BotManager
+    class AIMView(IMView):
+        """Sends out commit messages on AIM
+    
+        This view has three properties:
+    
+         - screenname - the AIM screenname to login with
+         - password - the password for AIM screenname
+         - to - a comma-seperated list of other AIM screennames to send the message to
+        """
+    
+        def execute(self):
+            """Sends the IM with the commit message"""
+    
+            bm = BotManager()
+            bot = TocTalk(self.screenname, self.password)
+    
+            bm.addBot(bot, "bot")
+            time.sleep(4)
+    
+            message = self._generateMessage()
+    
+            names = [name.strip() for name in self.to.split(',')]
+            for name in names:
+                print name
+                bot.do_SEND_IM(name, message)
+except:
+    pass    
